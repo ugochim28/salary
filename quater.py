f4777480513baf0ba917b5fc7564a9e3fe69e0d3
ugochim28 <ugochukwuuwandu28@gmail.com>
2024-09-19 20:48:20 +0200

    first feature branch commit

---
+++ quater.py
@@ -0,0 +1,20 @@
+# calculate the annual salary of an employee
+
+
+# promt user to enter their name
+
+# prompt user to enter their monthly salary
+
+# calculate the annual salary by multiplying monthly salary by 12
+
+# display the annual salary
+
+name = input("Please enter your name")
+
+salary = input("Please enter your monthly salary, i.e 5000")
+
+salary = float(salary)
+
+quartely_salary = salary * 3
+
+print(f"Dear {name} your quartely salary is {quartely_salary}")
